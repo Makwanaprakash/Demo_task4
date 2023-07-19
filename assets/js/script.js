e39fd693f6e0c07918b18357ec73160a76326048
Makwanaprakash <86949619+Makwanaprakash@users.noreply.github.com>
2023-07-19 23:02:42 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// banner slider js start
+$('.slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: false,
+  prevArrow: '<div class="slick-prev"><span><i class="fa fa-angle-left" aria-hidden="true"></i></span></div>',
+  nextArrow: '<div class="slick-next"><span><i class="fa fa-angle-right" aria-hidden="true"></i></span></div>'
+});
